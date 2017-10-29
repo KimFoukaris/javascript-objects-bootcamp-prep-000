ab3f2a89ed1ab89355ded249788a58ebe10528c4
Kim Foukaris <kimfoukaris@outlook.com>
2017-10-29 05:17:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {pink: "What about us?"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
+  artistName = 'artistName'
   playlist.artistName = songTitle
   return playlist
 }
